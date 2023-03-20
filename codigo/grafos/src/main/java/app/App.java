e41ed17ad1513c68a0a9124705c38261919f0f7f
DrumondGit <drumond.work@gmail.com>
2023-03-20 08:09:23 -0300

    metodo carregar feito

---
+++ codigo/grafos/src/main/java/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

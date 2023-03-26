26f1c24a373a59eeddd5a2c6fb4b0c14b32a89af
DrumondGit <drumond.work@gmail.com>
2023-03-26 12:13:24 -0300

    Metodos add vertice e remove Vertice de grafo mutavel feito

--- codigo/grafos/src/main/java/app/GrafoMutavel.java
+++ codigo/grafos/src/main/java/app/GrafoMutavel.java
@@ -5,26 +5,30 @@ import java.io.FileReader;
 
 public abstract class GrafoMutavel extends Grafo {
 
+    private ABB<Vertice> vertices;  // *Duvida
 
     public GrafoMutavel(String nome){
 
         super(nome);
     }
 
-    // Implementar
-    public boolean addVertice(int id) {
+    // Implementar *Duvida
 
-       return true; 
+    public boolean addVertice(int id){
+        Vertice novo = new Vertice(id);
+        return this.vertices.add(id, novo);
     }
 
-    // Implementar
+    // Implementar *Duvida
+
     public Vertice removeVertice(int id){
-        return null;
+        
+        return this.vertices.remove(id);
     }
 
-    public abstract boolean addAresta(int origem, int destino, int peso);
+    public abstract boolean addAresta(int origem, int destino, int peso);       // *Duvida
 
-    public abstract Aresta removeAresta(int origem, int destino);
+    public abstract Aresta removeAresta(int origem, int destino);               //  *Duvida
 
     public void carregar(String nomeArquivo){
 
